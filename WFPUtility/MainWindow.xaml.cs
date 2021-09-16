97bc99cb278323757391a3f6f0f02f37d6804213
waylen25 <waylen.jackson25@gmail.com>
2021-09-16 16:52:01 +0200

    Event handler functionality added to select a file and save data buttons in MainWindows.xaml.cs

--- WFPUtility/MainWindow.xaml.cs
+++ WFPUtility/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,46 @@ namespace WFPUtility
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnSelectFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            //Launch OpenFileDialog by ShowDialog method
+            Nullable<bool> result = openFileDialog.ShowDialog();
+
+            // Get the selected file name and display in a TextBox.
+            // Load content of file in a TextBlock
+            if (result == true)
+            {
+                txtEditor.Text = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+
+        private void btnSaveFile_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.DefaultExt = ".txt"; // Default file extension
+            saveFileDialog.Filter = "Text documents (.txt)|*.txt |Excel workbook (.xlsx)|*.xlsx|All files (*.*)|*.*"; // Filter files by extension
+            saveFileDialog.AddExtension = true;
+            saveFileDialog.OverwritePrompt = true;
+            
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, txtEditor.Text);
+
+                string messageBoxText = "File saved";
+                string caption = "File saved";
+                MessageBoxButton button = MessageBoxButton.OK;
+                MessageBoxImage icon = MessageBoxImage.Information;
+                MessageBoxResult result;
+
+                result = MessageBox.Show(messageBoxText, caption, button, icon, MessageBoxResult.OK);
+            }
+
+            
         }
     }
 }
